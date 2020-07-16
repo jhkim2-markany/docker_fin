82f6219c6f7d688ec82e0ea677468d09d164f8da
김준호 <enddl0121@gmail.com>
2020-07-16 15:29:19 +0900

    setupProxy 수정과 client 도커파일 ci  삭제

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -5,9 +5,9 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({ //프론트엔드 포트번호 3000번에서 5000으로 주겠다
-      target: `http://${ip.address()}:5000`,   //이거 일단 못받음
+      target: `http://${ip.address()}:5000`,   //도커용?
       // target: 'http://192.168.99.100:5000',
-      // target: 'http://localhost:5000',
+      // target: 'http://localhost:5000',      //로컬용
       changeOrigin: true,
     })
   );
